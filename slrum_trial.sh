c4cd22ea774c2ac31da9c7a079b34f07e376c138
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-02-16 02:03:59 +0530

    Create slrum_trial.sh

---
+++ slrum_trial.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+#SBATCH --account=def-drod1901
+#SBATCH --time=0-10:00:0
+#SBATCH --cpus-per-task=8
+
+
+
+pip install --no-index -r ~/VDSP_ocl/requirements.txt
+
+
+nnictl create --config config.yml
+nnictl experiment export --filename nni_log.csv --type csv
