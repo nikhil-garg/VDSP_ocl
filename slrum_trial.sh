a76ccc824749a67c35ad2da90203023e7b092504
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-02-16 02:29:12 +0530

    update

--- slrum_trial.sh
+++ slrum_trial.sh
@@ -4,9 +4,17 @@
 #SBATCH --cpus-per-task=8
 
 
+cd $SLURM_TMPDIR
 
+module load python/3.6 
+
+virtualenv --no-download $SLURM_TMPDIR/env  # SLURM_TMPDIR is on the compute node
+
+source $SLURM_TMPDIR/env/bin/activate
+
+
+git clone https://github.com/nikhil-garg/VDSP_ocl.git
+cd VDSP_ocl
 pip install --no-index -r requirements.txt
 
-
-nnictl create --config config.yml
-nnictl experiment export --filename nni_log.csv --type csv
+python mnist_multiple_exploration.py
