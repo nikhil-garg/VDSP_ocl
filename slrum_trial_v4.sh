48b2ce07a4b96e07307dd24d496c4fc918126d17
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-02-22 17:04:26 +0530

    updates

--- slrum_trial_v4.sh
+++ slrum_trial_v4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-drod1901
 #SBATCH --time=1-12:0:0
-#SBATCH --cpus-per-task=8 
+#SBATCH --cpus-per-task=2 
 #SBATCH --mem=32G
 OUTDIR=~/project/out/$SLURM_JOB_ID
 mkdir -p $OUTDIR
