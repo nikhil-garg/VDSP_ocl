f1aab37af06808d8da14a5b09f511288d136e8ad
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-02-28 01:24:24 +0530

    Update mnist_multiple_exploration_v2.py

--- mnist_multiple_exploration_v2.py
+++ mnist_multiple_exploration_v2.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 
 
 	parameters = dict(
-		vprog = [-0.60]
+		vprog = [-0.55, -0.60, -0.65]
 		,vthp=[0.25]
 		,input_nbr=[60000]
 		,g_max=[1/210]
@@ -60,7 +60,7 @@ if __name__ == '__main__':
 		,iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [30, 50, 70, 100, 150]
+		, n_neurons = [30, 50, 70, 100]
 		, inhibition_time = [10]
     )
 	param_values = [v for v in parameters.values()]
