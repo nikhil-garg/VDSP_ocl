235dce4f2325e7fff38ec1fe7ff32db042ddf1ab
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-03 15:49:26 +0530

    updates

--- qmnist_multiple_exploration.py
+++ qmnist_multiple_exploration.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
 		,iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [10, 20, 30,40,  50,60,70,80,90 100]
+		, n_neurons = [10, 20, 30,40,  50,60,70,80,90, 100]
 		, inhibition_time = [10]
     )
 	param_values = [v for v in parameters.values()]
