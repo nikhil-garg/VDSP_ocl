92596a3064960a7d8240ad9cfffb0319be211607
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-04 00:24:38 +0530

    updates

--- mnist_multiple_exploration_v2.py
+++ mnist_multiple_exploration_v2.py
@@ -50,13 +50,12 @@ if __name__ == '__main__':
 
 
 	parameters = dict(
-		vprog = [-0.35,-0.4,-0]
-		,vthp=[0.25]
+		vprog = [-0.35,-0.4,-0.45,-0.5,-0.55,-0.6]
 		,input_nbr=[60000]
 		,g_max=[1/210]
-		,tau_in = [0.1]
-		,tau_out = [0.1]
-		, lr = [0.0005, 0.001]
+		,tau_in = [0.06]
+		,tau_out = [0.06]
+		, lr = [0.0005]
 		,iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
@@ -69,7 +68,7 @@ if __name__ == '__main__':
 	folder = os.getcwd()+"/MNIST_VDSP_explorartion"+now
 	os.mkdir(folder)
 
-	for args.vprog,args.vthp,args.input_nbr,args.g_max,args.tau_in,args.tau_out,args.lr,args.iterations,args.presentation_time, args.dt,args.n_neurons,args.inhibition_time in product(*param_values):
+	for args.vprog,args.input_nbr,args.g_max,args.tau_in,args.tau_out,args.lr,args.iterations,args.presentation_time, args.dt,args.n_neurons,args.inhibition_time in product(*param_values):
 
 		args.filename = 'vprog-'+str(args.vprog)+'-g_max-'+str(args.g_max)+'-tau_in-'+str(args.tau_in)+'-tau_out-'+str(args.tau_out)+'-lr-'+str(args.lr)+'-presentation_time-'+str(args.presentation_time)
 		
@@ -79,8 +78,6 @@ if __name__ == '__main__':
 		pwd = os.getcwd()
 
 
-
-		args.vthn = args.vthp
 		accuracy, weights = evaluate_mnist_multiple(args)
 
 
--- slrum_trial_v2.sh
+++ slrum_trial_v2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-drod1901
-#SBATCH --time=4-23:58:0
-#SBATCH --cpus-per-task=4 
+#SBATCH --time=0-23:58:0
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
 OUTDIR=~/project/out/$SLURM_JOB_ID
 mkdir -p $OUTDIR
