48e77056438ebe6c7834dab81aaf63d9520e403b
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-11 23:30:26 +0530

    updates

--- mnist_multiple_exploration_v4.py
+++ mnist_multiple_exploration_v4.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
 		,inhibition_time = [10]
 		, lr = [0.1]
 		, presentation_time = [0.35]
-		, var_ratio = [0.8,0.9,1]
+		, var_ratio = [0.5,0.6,0.7]
 		, seed = [0,100,200,300,400,500,600,700,800,900]
     )
 	param_values = [v for v in parameters.values()]
