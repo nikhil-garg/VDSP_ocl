86910d836e8f5c72e1d63cd252845ae316c790c2
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-14 03:09:33 +0530

    updates

--- mnist_multiple_exploration_v1.py
+++ mnist_multiple_exploration_v1.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 		,iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [20,30]
+		, n_neurons = [10,40]
 		, seed = [0]
 		, inhibition_time = [10]
     )
