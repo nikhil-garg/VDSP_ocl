024a1975555b04b47c509fa226f55ad85dc0901e
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-18 13:28:29 +0530

    updates

--- mnist_multiple_exploration_v5.py
+++ mnist_multiple_exploration_v5.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 		, presentation_time = [0.35]
 		, dt = [0.005]
 		, n_neurons = [20,30,40,50]
-		, seed = [100]
+		, seed = [400]
 		, inhibition_time = [10]
     )
 	param_values = [v for v in parameters.values()]
