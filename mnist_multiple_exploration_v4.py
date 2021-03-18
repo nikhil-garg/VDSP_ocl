3c195ba1795617e6818159d8abb34bc75caf0d2d
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-03-18 12:54:26 +0530

    updates

--- mnist_multiple_exploration_v4.py
+++ mnist_multiple_exploration_v4.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 		,iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [30,50,70]
+		, n_neurons = [60,70,80]
 		, seed = [100]
 		, inhibition_time = [10]
     )
--- mnist_multiple_exploration_v5.py
+++ mnist_multiple_exploration_v5.py
@@ -54,11 +54,11 @@ if __name__ == '__main__':
 		,input_nbr=[60000]
 		,tau_in = [0.06]
 		,tau_out = [0.06]
-		, lr = [0.1,0.25,1]
+		, lr = [0.1]
 		,iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [30,40]
+		, n_neurons = [20,30,40,50]
 		, seed = [100]
 		, inhibition_time = [10]
     )
