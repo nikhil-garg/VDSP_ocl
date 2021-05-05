42aa777c5533d4002a0ae3a50900d58bcb77dde0
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-05-05 23:13:39 +0530

    updates

--- mnist_multiple_exploration_tio2_v4.py
+++ mnist_multiple_exploration_tio2_v4.py
@@ -51,16 +51,16 @@ if __name__ == '__main__':
 		df.to_csv(log_dir+'test.csv', index=False)
 
 	parameters = dict(
-		vprog = [-0.965]
-		, amp_neuron=[0.07]
+		vprog = [-0.925,-0.9]
+		, amp_neuron=[0.2,0.19,0.18,0.17,0.16]
 		,input_nbr=[60000]
-		,tau_in = [0.06]
-		,tau_out = [0.03]
+		,tau_in = [0.09,0.06,0.03]
+		,tau_out = [0.03,0.06]
 		, lr = [0.1]
-		, iterations=[2]
+		, iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [30, 50, 70]
+		, n_neurons = [30]
 		, inhibition_time = [10]
 		,seed =[100]
     )
--- mnist_multiple_exploration_tio2_v5.py
+++ mnist_multiple_exploration_tio2_v5.py
@@ -51,16 +51,16 @@ if __name__ == '__main__':
 		df.to_csv(log_dir+'test.csv', index=False)
 
 	parameters = dict(
-		vprog = [-0.965]
-		, amp_neuron=[0.07]
+		vprog = [-0.965,-0.95]
+		, amp_neuron=[0.15,0.14,0.13,0.009,0.008,0.007]
 		,input_nbr=[60000]
-		,tau_in = [0.06]
-		,tau_out = [0.03]
+		,tau_in = [0.09,0.06,0.03]
+		,tau_out = [0.03,0.06]
 		, lr = [0.1]
-		, iterations=[2]
+		, iterations=[1]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [40,60,80]
+		, n_neurons = [30]
 		, inhibition_time = [10]
 		,seed =[100]
     )
