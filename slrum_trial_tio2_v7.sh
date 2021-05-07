fac4f8ffad3a6afaf2b0b9e71a5a699c1b703923
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-05-07 20:39:37 +0530

    Update slrum_trial_tio2_v7.sh

--- slrum_trial_tio2_v7.sh
+++ slrum_trial_tio2_v7.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-drod1901
 #SBATCH --time=0-23:58:0
 #SBATCH --cpus-per-task=2 
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 OUTDIR=~/project/out/$SLURM_JOB_ID
 mkdir -p $OUTDIR
 cd $SLURM_TMPDIR
