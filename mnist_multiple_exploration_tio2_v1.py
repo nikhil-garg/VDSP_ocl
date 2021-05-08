d3d79920a12e7c5d2f72c1fc019456e3920bc8ee
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-05-09 03:15:42 +0530

    Update mnist_multiple_exploration_tio2_v1.py

--- mnist_multiple_exploration_tio2_v1.py
+++ mnist_multiple_exploration_tio2_v1.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 		df.to_csv(log_dir+'test.csv', index=False)
 
 	parameters = dict(
-		vprog = [-0.9,-0.875,-0.85]
+		vprog = [-0.9,-0.875]
 		, amp_neuron=[0.075,0.1]
 		,input_nbr=[60000]
 		,tau_in = [0.06]
