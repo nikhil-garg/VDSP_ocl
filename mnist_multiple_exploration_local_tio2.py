a1638ac3f8221c76075fe6442fbcf62f26eba5e0
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-05-11 04:03:56 +0530

    updates

--- mnist_multiple_exploration_local_tio2.py
+++ mnist_multiple_exploration_local_tio2.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
 		,tau_out = [0.06]
 		, lr = [1]
 		, iterations=[1]
-		, presentation_time = [0.35,0.3,0.4,0.25]
+		, presentation_time = [0.35]
 		, dt = [0.005]
 		, n_neurons = [30]
 		, inhibition_time = [10]
