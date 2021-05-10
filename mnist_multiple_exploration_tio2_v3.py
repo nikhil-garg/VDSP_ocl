ec96c2aafd0cc3614c1ba7d9ea8bda3b26606c17
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-05-10 17:59:30 +0530

    updates

--- mnist_multiple_exploration_tio2_v3.py
+++ mnist_multiple_exploration_tio2_v3.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
 		, iterations=[2]
 		, presentation_time = [0.35]
 		, dt = [0.005]
-		, n_neurons = [300]
+		, n_neurons = [500]
 		, inhibition_time = [10]
 		, tau_ref = [0.002]
 		, seed =[100]
