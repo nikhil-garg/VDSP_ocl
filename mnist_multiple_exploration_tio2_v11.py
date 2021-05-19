9c54e6b6737224b262b4065d313a82b5d0a00238
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-05-20 01:22:44 +0530

    updates

--- mnist_multiple_exploration_tio2_v11.py
+++ mnist_multiple_exploration_tio2_v11.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
 	parameters = dict(
 		vprog = [0]
-		, amp_neuron=[0.5]
+		, amp_neuron=[0.75]
 		,input_nbr=[60000]
 		,tau_in = [0.1,0.06,0.03]
 		,tau_out = [0.03]
