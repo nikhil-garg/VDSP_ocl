aae045ffea9e60c7fed7f47e1ecdad025f779754
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-05-21 09:33:43 +0530

    updates

--- mnist_multiple_exploration_tio2_v7.py
+++ mnist_multiple_exploration_tio2_v7.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
 		, amp_neuron=[0.5,0.75]
 		,input_nbr=[60000]
 		,tau_in = [0.1]
-		,tau_out = [0.06]
+		,tau_out = [0.03]
 		, lr = [1]
 		, iterations=[1]
 		, presentation_time = [0.2]
