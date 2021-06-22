2cc482654c9cd78bcfe33ac71f167c64cd1d337b
Nikhil <f2016597@goa.bits-pilani.ac.in>
2021-06-22 23:14:07 +0530

    Update mnist_multiple_exploration_v11.py

--- mnist_multiple_exploration_v11.py
+++ mnist_multiple_exploration_v11.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 		, presentation_time = [0.35]
 		, pause_time = [0]
 		, dt = [0.005]
-		, n_neurons = [200]
+		, n_neurons = [10,20,30]
 		, inhibition_time = [10]
 		, tau_ref_in = [0.005]
 		, tau_ref_out = [0.005]
@@ -91,7 +91,7 @@ if __name__ == '__main__':
 		, gain_in = [4]
 		, bias_in = [0.5]
 		, noise_input = [0]
-		, seed =[100]
+		, seed =[50]
     )
 	param_values = [v for v in parameters.values()]
 
