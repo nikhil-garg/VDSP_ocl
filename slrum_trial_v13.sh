3dff67dea0b2f9f7940c65a4c9d495593d1cc811
Nikhil Garg <f2016597@goa.bits-pilani.ac.in>
2021-07-07 19:38:38 +0530

    updates

--- slrum_trial_v13.sh
+++ slrum_trial_v13.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-drod1901
-#SBATCH --time=0-23:58:0
-#SBATCH --cpus-per-task=2 
-#SBATCH --mem=32G
+#SBATCH --time=5-23:58:0
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
 OUTDIR=~/project/out/$SLURM_JOB_ID
 mkdir -p $OUTDIR
 cd $SLURM_TMPDIR
